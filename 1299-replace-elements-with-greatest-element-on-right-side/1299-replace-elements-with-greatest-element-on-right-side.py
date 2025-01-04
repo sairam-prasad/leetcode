a9682b19aeeeb701afd1e061822ea42305424925
sairam-prasad <50474319+sairam-prasad@users.noreply.github.com>
2025-01-03 22:25:18 -0500

    Time: 11 ms (95.89%), Space: 18.5 MB (31.89%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -6,7 +6,7 @@ class Solution:
         temp_max=0
 
         
-        for i in range(len(arr)-1,-1,-1):
+        for i in range(len(arr)-2,-1,-1):
             
             if arr[i]>maxi:
                 temp_max = maxi
